cb8297a34e1a5b80fc012a091eff9b8b757187b5
nayaa <yaana@gmail.com>
2024-12-26 14:19:15 +0900

    회원 가입 이벤트 추가 및 처리

--- app/src/main/java/nayeon/memberapplication/MainActivity.java
+++ app/src/main/java/nayeon/memberapplication/MainActivity.java
@@ -1,6 +1,11 @@
 package nayeon.memberapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.accessibility.AccessibilityManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +13,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import nayeon.memberapplication.helper.DatabaseHelper;
+
 public class MainActivity extends AppCompatActivity {
+
+    // 변수 선언
+    private EditText editTextUserid, editTextPasswd, editTextName, editTextEmail;
+    private Button buttonJoin, buttonUserlist;
+    private DatabaseHelper databaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +32,61 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // view 초기화
+        editTextUserid = findViewById(R.id.editTextUserid);
+        editTextPasswd = findViewById(R.id.editTextPassword);
+        editTextName = findViewById(R.id.editTextName);
+        editTextEmail = findViewById(R.id.editTextEmail);
+        buttonJoin = findViewById(R.id.buttonJoin);
+        buttonUserlist = findViewById(R.id.buttonUserlist);
+
+        // DB Helper 초기화
+        databaseHelper = new DatabaseHelper(this);
+
+        // 회원가입 이벤트 처리
+        buttonJoin.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        registerUser();
+                    }
+                }
+        );
+
+    }
+
+    private void registerUser() {
+        // 변수 초기화
+        String userid = editTextUserid.getText().toString().trim();
+        String passwd = editTextPasswd.getText().toString().trim();
+        String name = editTextName.getText().toString().trim();
+        String email = editTextEmail.getText().toString().trim();
+        
+        // 입력값 검증
+        if(userid.isEmpty() || passwd.isEmpty() || name.isEmpty() || email.isEmpty()){
+            Toast.makeText(this, "모든 정보 입력 바람", Toast.LENGTH_SHORT).show();
+            return; // 여기서 중지
+        }
+        
+        // 중복 아이디 체크
+
+
+        // 회원 저장
+        boolean success = databaseHelper.insertMember(userid, passwd, name, email);
+        if(success){
+            Toast.makeText(this, "💖 회원가입 성공 💖", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "😓 다시 시도해보세요 😓", Toast.LENGTH_SHORT).show();
+        }
     }
 }
+
+
+
+
+
+
+
+
+
